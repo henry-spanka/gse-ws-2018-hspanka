9000f11799911d0ae2b8979ae5d4c03f0925236a
Henry Spanka <hspanka@techfak.uni-bielefeld.de>
2018-11-03 13:13:11 +0100

    exit application if an empty argument is supplied - closes #51393 @5m

--- src/main/java/de/techfak/gse/hspanka/ChessGame.java
+++ src/main/java/de/techfak/gse/hspanka/ChessGame.java
@@ -4,6 +4,12 @@ public class ChessGame {
     public static void main(final String... args) {
         BoardController boardController = new BoardController();
 
+        if (args.length > 0) {
+            if (args[0].isEmpty()) {
+                System.exit(0);
+            }
+        }
+
         boardController.showCurrentBoard();
     }
 
