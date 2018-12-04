77031858a6add20023b714030570dd0718c8f36f
Henry Spanka <hspanka@techfak.uni-bielefeld.de>
2018-12-04 18:05:19 +0100

    created gui layout for menu - closes #55128 @15m

--- src/main/java/de/techfak/gse/hspanka/app/ChessGameGUIApplication.java
+++ src/main/java/de/techfak/gse/hspanka/app/ChessGameGUIApplication.java
@@ -1,15 +1,22 @@
 package de.techfak.gse.hspanka.app;
 
+import de.techfak.gse.hspanka.BoardController;
 import de.techfak.gse.hspanka.exceptions.ApplicationErrorException;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class ChessGameGUIApplication extends Application implements ChessGameApplication {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Scene scene = new Scene(new VBox());
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("app.fxml"));
+
+        Pane root = fxmlLoader.load();
+        BoardController boardController = fxmlLoader.getController();
+
+        Scene scene = new Scene(root);
         primaryStage.setTitle("Chess Game");
         primaryStage.setScene(scene);
         primaryStage.show();
